331dd4138ac983834e771dd3e1c1e0497f36616b
Nginx Ninja <dummy.kenmen2@gmail.com>
2019-02-18 16:08:29 +0800

    Ends at page 94. 13.68% completed. Add exit; command on the condition. Refactor codes in Chapter02 files.
     Changes to be committed:
    	modified:   Chapter01/processorder.php
    	new file:   Chapter02/orderform.html
    	new file:   Chapter02/processorder.php

--- Chapter02/processorder.php
+++ Chapter02/processorder.php
@@ -10,12 +10,8 @@ $tireqty = (int)($_POST['tireqty']);
 $oilqty = (int)($_POST['oilqty']);
 $sparkqty = (int)($_POST['sparkqty']);
 $find = $_POST['find'];
+$address = $_POST['address'];
 
-/** This variables are strings from the $_POST[] array.
-$tireqty = $_POST['tireqty'];
-$oilqty = $_POST['oilqty'];
-$sparkqty = $_POST['sparkqty'];
-**/
 ?>
 
 <!DOCTYPE html>
@@ -30,13 +26,6 @@ $sparkqty = $_POST['sparkqty'];
     	$totalqty = $tireqty + $oilqty + $sparkqty;
     	
     	if($totalqty == 0){
-    	    ## Checking the Variable Type
-    	    echo '<p>';
-    	    echo '$totalqty: ' . gettype($totalqty) . '<br/>';
-    	    echo '$tireqty: ' . gettype($tireqty) . '<br/>';
-    	    echo '$oilqty: ' . gettype($oilqty) . '<br/>';
-    	    echo '$sparkqty: ' . gettype($sparkqty) . '<br/>';
-    	    echo '</p>';
     	    echo '<p style = "color: red;">';
     	    echo 'You did not order anything from the previous page! </p>';
     	    exit;
@@ -66,12 +55,6 @@ $sparkqty = $_POST['sparkqty'];
     	echo '</p>';
     	
     	echo '<p>Your order is as follows: </p>';
-    	# (shell script comment) This is the same output as the variable assignment.
-    	echo '<strong><u>Calling the $_POST[] array</u></strong><br/>';
-    	echo htmlspecialchars($_POST['tireqty']) . ' tires. <br/>';
-    	echo htmlspecialchars($_POST['oilqty']) . ' bottles of oil. <br/>';
-    	echo htmlspecialchars($_POST['sparkqty']) . ' spark plugs. <br/>';
-        echo "<p> </p>";
     	
         echo '<strong><u>Calling the variable assignment</u></strong><br/>';
     	echo htmlspecialchars($tireqty) . ' tires. <br/>';
@@ -82,7 +65,6 @@ $sparkqty = $_POST['sparkqty'];
     	echo "<p>";
     	echo "Items ordered: $totalqty. <br/>";
 
-//    	$totalamount = 0.00;
     	$totalamount = $tireqty * TIREPRICE +
     	               $oilqty * OILPRICE +
     	               $sparkqty * SPARKPRICE;
@@ -95,21 +77,7 @@ $sparkqty = $_POST['sparkqty'];
     	
     	echo "</p>";
     	
-    	# (shell script comment) Heredoc sysntax
-    	echo <<<theString
-    	<br/> This is
-        a sample heredoc string.
-        The End.
-theString;
-    	# Testing Variable Status
-    	echo "<p>";
-    	echo "<strong>Testing the Variable Status</strong> <br/>";
-    	echo 'isset($tireqty): ' . isset($tireqty) . '<br/>';
-    	echo 'isset($nothere): ' . isset($nothere) . '<br/>';
-    	echo 'empty($tireqty): ' . empty($tireqty) . '<br/>';
-    	echo 'empty($nothere): ' . empty($nothere) . '<br/>';
-    	echo "</p>";
-    	}
+    	echo "Address to ship to is ".$address.".";
     	?>
     </body>
 </html>
