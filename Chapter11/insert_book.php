dc09082c0ef49c5461065f19f34451004f1890dc
Nginx Ninja <dummy.kenmen2@gmail.com>
2019-03-19 05:20:46 +0800

    Ends at page 346. 50.36% completed.
     Changes to be committed:
    	modified:   Chapter11/insert_book.php

--- Chapter11/insert_book.php
+++ Chapter11/insert_book.php
@@ -15,11 +15,41 @@
     **/
     
     // This is still not working. Please FIX this before proceeding.
-    if(($_POST['ISBN'] || $_POST['Author'] || $_POST['Title'] || $_POST['Price']) == NULL){
+    if(($_POST['ISBN']== NULL) || ($_POST['Author']== NULL) || ($_POST['Title']== NULL) || ($_POST['Price']== NULL)){
         echo "<p>You have not entered all the required details.<br />
              Please go back and try again.</p>";
         exit;
     }
+    
+    //create short variable names
+    $isbn = $_POST['ISBN'];
+    $author = $_POST['Author'];
+    $title = $_POST['Title'];
+    $price = $_POST['Price'];
+    $price = doubleval($price);
+    
+    $db = new mysqli('localhost', 'bookorama', 'bookorama123', 'books');
+    
+    if(mysqli_connect_errno()){
+        echo "<p>Error: Could not connect to database.<br/>
+             Please try again later.</p>";
+        exit;
+    }
+    
+    $query = "INSERT INTO Books VALUES(?, ?, ?, ?)";
+    $stmt = $db->prepare($query);
+    $stmt->bind_param('sssd', $isbn, $author, $title, $price);
+    $stmt->execute();
+    
+    if($stmt->affected_rows > 0){
+        echo '<p>Book inserted into the database.</p>';
+    }
+    else{
+        echo '<p>An error has occured. <br />
+              The item was not added.</p>';
+    }
+    
+    $db->close();
     
     ?>
 </body>
